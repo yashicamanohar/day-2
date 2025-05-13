ab8ab9bf117db887659d1027d146ad47ece9310c
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-13 10:25:37 +0530

    Add files via upload

---
+++ name age salary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   char name[10];
+   printf("Enter your name :");
+   scanf("%s",&name);
+  printf("%s",name);
+   int age;
+   printf("Enter you age :");
+   scanf("%d",&age);
+   printf("%d",age);
+   float salary;
+   printf("Enter your salary:");
+   scanf("%f",&salary);
+   printf("%f",salary);
+ }
